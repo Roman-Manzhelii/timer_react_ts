43be2f25d235b7dd3725bdc7d36dd48cbe8c867b
Roman Manzhelii <145562170+Roman-Manzhelii@users.noreply.github.com>
2025-08-03 17:33:23 +0100

    make start/stop button works

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,24 +1,32 @@
 import Container from './UI/Container.tsx';
-import { type Timer as TimerProps} from '../store/timers-context.tsx'
+import { useTimersContext, type Timer as TimerProps} from '../store/timers-context.tsx'
 import { useEffect, useRef, useState } from 'react';
 
 export default function Timer({ name, duration }: TimerProps) {
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
   const intervalRef = useRef<number | null>(null);
-  
+  const {isRunning} = useTimersContext();
+
   if(remainingTime <= 0 && intervalRef.current) {
     clearInterval(intervalRef.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(function () {
-      setRemainingTime(prevTime => prevTime - 50);
-    }, 50);
-    intervalRef.current = timer
+    let timer: number;
+    
+    if(isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime(prevTime => prevTime - 50);
+      }, 50);
+      intervalRef.current = timer
+    } else if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
     return () => {
       clearInterval(timer);
     }
-  }, []);
+  }, [isRunning]);
   
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
